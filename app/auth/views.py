cd70114d562f4b3760471a71cf8415aa704158b4
Anton Kovalchuk <koval400k@gmail.com>
2019-10-26 21:57:48 +0200

    registration

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, redirect, request, url_for, flash
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 from ..models import User
 from flask_login import login_user, logout_user, login_required
+from .. import db
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -14,6 +15,20 @@ def login():
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password.')
     return render_template('auth/login.html', form=form)
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('YOU can now login.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
 
 
 @auth.route('/logout')
