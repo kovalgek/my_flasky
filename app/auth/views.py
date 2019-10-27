b754c0fa6319829d206afdd4fafffdcb5c9bd535
Anton Kovalchuk <koval400k@gmail.com>
2019-10-27 15:15:31 +0100

    send confirmation email

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,8 +2,9 @@ from flask import render_template, redirect, request, url_for, flash
 from . import auth
 from .forms import LoginForm, RegistrationForm
 from ..models import User
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from .. import db
+from ..email import send_email
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -26,10 +27,22 @@ def register():
                     password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('YOU can now login.')
+        token = user.generate_confirmatin_token()
+        send_email(user.email, 'Confirm your account', 'auth/email/confirm', user=user, token=token)
+        flash('A confirmation email has been sent to you by email.')
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
 
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
 
 @auth.route('/logout')
 @login_required
--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
