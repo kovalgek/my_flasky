e523e4d3bd7b5999c17cf57e535287d8edbbe3af
Anton Kovalchuk <koval400k@gmail.com>
2019-12-11 23:15:45 +0100

    add pagination

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, abort, flash, redirect, url_for
+from flask import render_template, abort, flash, redirect, url_for, request, current_app
 from . import main
 from flask_login import login_required, current_user
 from ..decorators import admin_required, permission_required
@@ -17,8 +17,10 @@ def index():
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('.index'))
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', form=form, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'], error_out=False)
+    posts = pagination.items
+    return render_template('index.html', form=form, posts=posts, pagination=pagination)
 
 @main.route('/admin')
 @login_required
@@ -37,7 +39,8 @@ def user(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         abort(404)
-    return render_template('user.html',user=user)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html',user=user, posts=posts)
 
 @main.route('/edit-profile', methods=['GET','POST'])
 @login_required
