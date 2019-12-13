405a383cf496217496a012d6151f63624b16145d
Anton Kovalchuk <koval400k@gmail.com>
2019-12-13 22:39:29 +0100

    add link to post

--- app/main/views.py
+++ app/main/views.py
@@ -22,6 +22,12 @@ def index():
     posts = pagination.items
     return render_template('index.html', form=form, posts=posts, pagination=pagination)
 
+@main.route('/post/<int:id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    print('post',post)
+    return render_template('post.html', posts=[post])
+
 @main.route('/admin')
 @login_required
 @permission_required(Permission.ADMINISTER)
