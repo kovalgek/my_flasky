6bd31b314456ceca6ae040ba0da86ff4150842eb
Anton Kovalchuk <koval400k@gmail.com>
2019-12-25 16:43:37 +0100

    add api

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,11 +1,15 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from . import main
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return  render_template('404.html'), 404
+    if request.accept_mimetypes.accept_json and not request.accept_mimetypes.accept_html:
+        response = jsonify({'error':'not found'})
+        response.status_code = 404
+        return response
+    return render_template('404.html'), 404
 
 @main.app_errorhandler(500)
 def internal_service_error(e):
-    return  render_template('500.html'), 500
+    return render_template('500.html'), 500
 
